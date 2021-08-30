28d49a6079301afc3017503fedd1f783e7c76bd8
divanov11 <ivanovsin11@gmail.com>
2021-08-30 18:15:39 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+import Header from './components/Header'
+import Notes from './pages/Notes'
+import Note from './pages/Note'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Route path="/" exact component={Notes} />
+          <Route path="/note/:id" component={Note} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
